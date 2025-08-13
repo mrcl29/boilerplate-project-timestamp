0ee4133130c05b78600568c298864ccd5d5ca68d
marc <marc.llobera@alcort.es>
2025-08-13 13:55:41 +0200

    Update

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api', (req, res) => {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
+
 app.get('/api/:date', (req, res) => {
   const dateParam = req.params.date;
   let date;
